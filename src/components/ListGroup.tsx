6b83f6fd808fd0139de14556168b9dded09fcabd
Owais-AlShishani <98020718+Owais-AlShishani@users.noreply.github.com>
2024-08-18 15:53:56 +0300

    Conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,8 +1,10 @@
 function ListGroup() {
-  const items = ["Grozny", "New York", "Paris", "Amman"];
+  let items = ["Grozny", "New York", "Paris", "Amman"];
+  items = [];
   return (
     <>
       <h1>List</h1>
+      {items.length > 0 && <p>No Items Found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li className="list-group-item" key={item}>
