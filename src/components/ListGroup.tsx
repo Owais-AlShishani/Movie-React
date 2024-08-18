c06449e631aa4b2e469edd6ae49e4b32490cd07a
Owais-AlShishani <98020718+Owais-AlShishani@users.noreply.github.com>
2024-08-18 15:14:18 +0300

    Rendering List with map

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,17 @@
+function ListGroup() {
+  const items = ["Grozny", "New York", "Paris", "Amman"];
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
