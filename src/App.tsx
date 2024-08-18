fa1891e8e4108eff0c43718cce2438dcd43368d6
Owais-AlShishani <98020718+Owais-AlShishani@users.noreply.github.com>
2024-08-18 18:40:16 +0300

    Passing Children To Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
 import "./App.css";
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["Grozny", "New York", "Paris", "Amman"];
-  const handleSelectedItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectedItem}
-      />
+      <Alert>Hello World</Alert>
     </div>
   );
 }
