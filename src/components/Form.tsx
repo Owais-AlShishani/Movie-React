2512a71015c82f26ecdb3d3be6e8fb7e2bd29622
Owais-AlShishani <98020718+Owais-AlShishani@users.noreply.github.com>
2024-08-23 17:23:01 +0300

    Controlled Component
    useRef are more optimized but this approach is good

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,34 +1,27 @@
-import { useRef } from "react"
+import { useState } from "react"
 
 const Form = () => {
-    const nameRef = useRef<HTMLInputElement>(null);
-    const ageRef = useRef<HTMLInputElement>(null);
-    const person = {
-        name: '',
-        age: 0
-    }
+    const [person, setPerson] = useState({
+        name: "",
+        age: ""
+    })
 
     const handleSubmit = (event: { preventDefault: () => void }) => {
         event.preventDefault()
 
-        if (nameRef.current !== null)
-            person.name = nameRef.current.value
-
-        if (ageRef.current !== null)
-            person.age = parseInt(ageRef.current.value)
         console.log(person)
     }
     return (
         <form onSubmit={handleSubmit}>
-            {/* {referencing the function here not calling it} */}
+            {/* {referencing the function here not calling it like this ()} */}
             <div className="mb-3">
                 <label htmlFor="name" className="form-label">Name</label>
-                <input ref={nameRef} id='name' type="text" className="form-control" />
+                <input value={person.name} onChange={(event) => setPerson({ ...person, name: event.target.value })} id='name' type="text" className="form-control" />
             </div>
 
             <div className="mb-3">
                 <label htmlFor="age" className="form-label">Age</label>
-                <input ref={ageRef} id='age' type="number" className="form-control" />
+                <input value={person.age} onChange={(event) => setPerson({ ...person, age: parseInt(event.target.value) })} id='age' type="number" className="form-control" />
             </div>
             <button type="submit" className="btn btn-primary">Submit</button>
         </form>
