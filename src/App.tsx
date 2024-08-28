91f16b66ff84d8099b7db305dc67393a4fc170e5
Owais-AlShishani <98020718+Owais-AlShishani@users.noreply.github.com>
2024-08-28 13:20:53 +0300

    Added expenses list filter and add with validations

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,35 @@
 import { useState } from "react";
 import "./App.css";
-import Alert from "./components/Alert";
-import Button from "./components/Button/Button";
-import ListGroup from "./components/ListGroup/Index";
-import { BsCalendar2DateFill } from "react-icons/bs";
-import Like from "./components/Like";
-import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
-import Form from "./components/Form";
-
-
+import ExpenseList from "./components/Expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./components/Expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./components/Expense-tracker/components/ExpenseForm";
 function App() {
-  // const items = ['New York', 'San Francisco', 'Los Angeles']
-  //const [cartItems, setCartItems] = useState(['product1', 'product2']);
-  // const [game, setGame] = useState({
-  //   id: 1,
-  //   player: {
-  //     name: 'john'
-  //   }
-  // });
-  // const [pizza, setPizza] = useState({
-  //   name: 'Veggie',
-  //   toppings: ['Mushroom']
-  // });
 
-  const [cart, setCart] = useState({
-    discount: .1,
-    items: [
-      { id: 1, title: 'product1', quantity: 1 },
-      { id: 2, title: 'product2', quantity: 1 },
-      { id: 3, title: 'product3', quantity: 1 }
-    ]
-  })
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: 'descTest', amount: 10, category: 'Groceries' },
+    { id: 2, description: 'descTest2', amount: 5, category: 'Utilities' },
+    { id: 3, description: 'descTest3', amount: 7, category: 'Entertainment' },
+    { id: 4, description: 'descTest4', amount: 20, category: 'Entertainment' }
+  ]);
 
+  const [selectedCategory, setSelectedCategory] = useState('');
 
-  const handleClick = () => {
-    // setGame({ ...game, player: { ...game.player, name: 'Bob' } })
-    // console.log('game', game)
-    //setPizza({ ...pizza, toppings: [...pizza.toppings, 'Cheese'] })
-
-    //if the user choosed id nmuber 2 in items inside cart object it should update quantity to 2 could you do that for me?
-    setCart({ ...cart, items: cart.items.map(item => item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item) })
-
-  }
+  console.log(expenses)
+  const filteredCategory = selectedCategory
+    ? expenses.filter(e => e.category === selectedCategory)
+    : expenses;
 
   return (
     <>
-      {/* {console.log('Log', game)} */}
-      {/* <NavBar count={cartItems.length} />*/}
-      {/* <Cart cartItems={cartItems} onClear={handleClick} /> */}
-      <Form />
+      <div className="mb-4">
+        <ExpenseForm onSubmit={expense => setExpenses([...expenses, { id: expenses.length + 1, ...expense }])} />
+      </div>
+
+      <div className="mb-3">
+        <ExpenseFilter onSelectCategory={(category) => setSelectedCategory(category)} />
+      </div>
+
+      <ExpenseList expenses={filteredCategory} onDelete={(id) => setExpenses(expenses.filter(e => e.id !== id))} />
     </>
   );
 }
