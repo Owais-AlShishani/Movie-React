48ff0dafc592506e665a196186328de100b82d77
Owais-AlShishani <98020718+Owais-AlShishani@users.noreply.github.com>
2024-09-01 20:59:42 +0300

    Updating Data

--- src/App.tsx
+++ src/App.tsx
@@ -50,6 +50,18 @@ function App() {
         setUsers([...originalUsers]);
       })
   }
+
+  const updateUser = (user: IUser) => {
+    const originalUsers = [...users];
+    const updatedUser = { ...user, name: user.name + '!' };
+    setUsers(users.map(u => u.id === user.id ? updatedUser : u))
+    axios.patch('https://jsonplaceholder.typicode.com/users/' + user.id, updatedUser)
+      .catch(err => {
+        setError(err.message);
+        setUsers(originalUsers);
+      })
+  }
+
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
@@ -59,7 +71,10 @@ function App() {
         {users.map(user =>
           <li key={user.id} className="list-group-item d-flex justify-content-between">
             {user.name}
-            <button className="btn btn-outline-danger" onClick={() => deleteUser(user)}>Delete</button>
+            <div>
+              <button className="btn btn-secondary mx-1" onClick={() => updateUser(user)}>Update</button>
+              <button className="btn btn-outline-danger" onClick={() => deleteUser(user)}>Delete</button>
+            </div>
           </li>)}
       </ul >
     </>
